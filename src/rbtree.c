6ba455cf55fe78da787796f794a5e397b403c3e9
moonxxpower <su551732@gmail.com>
2024-04-22 20:20:27 +0900

    new_rbtree 구현

--- src/rbtree.c
+++ src/rbtree.c
@@ -2,11 +2,22 @@
 
 #include <stdlib.h>
 
+// 새로운 RB Tree 생성
 rbtree *new_rbtree(void) {
-  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
-  return p;
+  rbtree *t = (rbtree *)calloc(1, sizeof(rbtree));
+  
+  node_t *nil = (node_t *)calloc(1, sizeof(node_t));
+  nil -> color = RBTREE_BLACK;  // nil 노드는 항상 black
+
+  // 초기값 설정
+  t -> nil = t -> root = nil;
+
+  return t;
 }
+
+// void left_rotation(rbtree *t, node_t *p)
+
+// void right_rotation(rbtree *t, node_t *p)
 
 void delete_rbtree(rbtree *t) {
   // TODO: reclaim the tree nodes's memory
@@ -17,6 +28,8 @@ node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
   return t->root;
 }
+
+// rbtree_insert_fixup(rbtree *t, node_t *p)
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
   // TODO: implement find
@@ -37,6 +50,8 @@ int rbtree_erase(rbtree *t, node_t *p) {
   // TODO: implement erase
   return 0;
 }
+
+// rbtree_delte_fixup(rbtree *t, node_t *p)
 
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
   // TODO: implement to_array
