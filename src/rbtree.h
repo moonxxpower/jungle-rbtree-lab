c284d60076db24da04e2a2633465079f496ca37f
moonxxpower <su551732@gmail.com>
2024-04-24 07:34:57 +0900

    추가로 구현한 함수 추가

--- src/rbtree.h
+++ src/rbtree.h
@@ -19,14 +19,20 @@ typedef struct {
 } rbtree;
 
 rbtree *new_rbtree(void);
+void delete_node(rbtree *, node_t *);
 void delete_rbtree(rbtree *);
 
 node_t *rbtree_insert(rbtree *, const key_t);
+void rbtree_insert_fixup(rbtree *, node_t *);
 node_t *rbtree_find(const rbtree *, const key_t);
 node_t *rbtree_min(const rbtree *);
 node_t *rbtree_max(const rbtree *);
 int rbtree_erase(rbtree *, node_t *);
+void rbtree_erase_fixup(rbtree *, node_t *);
+void transplant(rbtree * , node_t * , node_t * );
+node_t * node_min(rbtree * , node_t * );
 
 int rbtree_to_array(const rbtree *, key_t *, const size_t);
+int node_to_array(const rbtree *, node_t *, key_t *, int);
 
 #endif  // _RBTREE_H_
